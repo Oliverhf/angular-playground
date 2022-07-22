0065efdc214365c8c08ef74d8dee48df3d7a77be
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-07-22 14:09:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+   title: string = "basket of baby beavers";
+   imgSrc: string = "https://townsquare.media/site/81/files/2021/06/attachment-Beaver-Photo-5.jpg?w=980&q=75"
+   numberOne: number = 1;
+   numberTwo: number = 2;
+   togglePokemon: boolean = true;
+   favoriteAnimal: string = "turtle";
+   pokemonName: string = "";
+
+  constructor() {
+    this.title = "Oliver";
+  }
+
+  handleClick(value: any) {
+    console.log(value);
+  }
 }
