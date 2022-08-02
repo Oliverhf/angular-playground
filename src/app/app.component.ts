e9ceeb6a1fa7240cdcd584f7005f6e8e397a7d8a
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-08-02 18:57:03 -0300

    ngFor Angular loop, ngClass Angular class and Modules (Largest separation of code in Angular) Lifecycle Method ngOnInit() (When the page first loads)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string,
+  isActive: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
    title: string = "basket of baby beavers";
    imgSrc: string = "https://townsquare.media/site/81/files/2021/06/attachment-Beaver-Photo-5.jpg?w=980&q=75"
@@ -14,6 +22,26 @@ export class AppComponent {
    togglePokemon: boolean = true;
    favoriteAnimal: string = "turtle";
    pokemonName2: string = "";
+   pokemons: Pokemon[] = [{
+    id: 1,
+    name: 'pikachu',
+    type: 'eletric',
+    isActive: true
+   }, 
+   {
+    id: 2,
+    name: 'squirtle',
+    type: 'water',
+    isActive: false
+   },
+   {
+    id: 3,
+    name: 'charmander',
+    type: 'fire',
+    isActive: false
+   },
+   
+  ];
 
   constructor() {
     this.title = "Oliver";
