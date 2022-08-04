fdc71174b0b14c5d1886bf032aa00dbd72dc90a7
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-08-04 17:17:19 -0300

    Added Json server and Observable

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,37 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
 import {Pokemon} from 'src/app/models/pokemon';
+import { Observable } from 'rxjs';
+
+const POKEMON_API = 'http://localhost:3000/pokemon'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-constructor() { }
+constructor(private http: HttpClient) { }
 
-getPokemons() : Pokemon[] {
-  return [{
-      id: 1,
-      name: 'pikachu',
-      type: 'eletric',
-      isActive: true,
-      isStylish: true
-    }, 
-    {
-      id: 2,
-      name: 'squirtle',
-      type: 'water',
-      isActive: false,
-      isStylish: false
-    },
-    {
-      id: 3,
-      name: 'charmander',
-      type: 'fire',
-      isActive: false,
-      isStylish: false
-    },
-    
-    ];
+getPokemons() : Observable<Pokemon[]> {
+  return this.http.get<Pokemon[]>(POKEMON_API);
 }
 
 }
