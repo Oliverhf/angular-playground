671a06359cb272bc994ac50d7a2d0c9bb8bc7a80
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-09-05 16:57:31 -0300

    Renderer2

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import {Pokemon} from 'src/app/models/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
@@ -13,14 +13,18 @@ export class PokemonListComponent implements OnInit, AfterViewInit {
   @ViewChildren('pokemonRef') pokemonRef!: ElementRef;
   @ViewChild('pokemonTh') pokemonTh!: ElementRef
 
-  constructor(private pokemonService: PokemonService) {
+  constructor(private pokemonService: PokemonService, private renderer: Renderer2) {
 
   }
 
 
   ngAfterViewInit(): void {
-    console.log(this.pokemonTh)
+    console.log(this.pokemonTh);
     this.pokemonTh.nativeElement.innerText = "Pokemon Name";
+    const div = this.renderer.createElement('div');
+    const text = this.renderer.createText('Pokemon List');
+    this.renderer.appendChild(div, text);
+    this.renderer.appendChild(this.pokemonTh.nativeElement, div);
   }
 
   handleRemove(event: Pokemon) {
